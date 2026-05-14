0f7b15c6cb5e8f4f6df2cd71abc2ee8f6997c6b1
Christian Duarte <chrustian.932000@gmail.com>
2026-05-14 14:51:36 +0000

    feat: adiciona leitura de coordenadas do jogador usando Scanner no Main

--- T2_POO/Main.java
+++ T2_POO/Main.java
@@ -1,23 +1,32 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
         Tabuleiro tabuleiro = new Tabuleiro(4, 4);
         String[] simbolos = {"A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H"};
-        Carta carta1 = new Carta(0, 0, "A");
-        Carta carta2 = new Carta(0, 1, "B");
-
-        System.out.println(carta1);
-        System.out.println(carta2);
-
-        carta1.setVirada(true);
-
-        System.out.println("Exibindo cartas:");
-        System.out.println(carta1.exibir());
-        System.out.println(carta2.exibir());
-
+        
         tabuleiro.inicializar(simbolos);
         System.out.println("Tabuleiro inicializado:");
-
         tabuleiro.exibir();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("\n--- Sua vez de jogar! ---");
+        
+        // le as coordenadas da primeira carta
+        System.out.print("Digite a linha da 1ª carta: ");
+        int linha1 = sc.nextInt(); 
+        System.out.print("Digite a coluna da 1ª carta: ");
+        int coluna1 = sc.nextInt(); 
+
+        // le as coordenadas da segunda carta
+        System.out.print("Digite a linha da 2ª carta: ");
+        int linha2 = sc.nextInt(); 
+        System.out.print("Digite a coluna da 2ª carta: ");
+        int coluna2 = sc.nextInt(); 
+        
+        System.out.println("Você quer virar as cartas nas posições: (" + linha1 + "," + coluna1 + ") e (" + linha2 + "," + coluna2 + ")");
+        
+        sc.close();
     }
 }
